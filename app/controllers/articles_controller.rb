951b636436afde7d96d0caa83c09d7f50404bf27
Tech-A25 <Tech-A25@Tech-A25s-MacBook-Air.local>
2019-01-04 22:47:23 +0000

    Added the comments and the update and delete functions with authentication to edit, create and delete post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
     def index
         @articles = Article.all
     end
@@ -8,14 +11,39 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
+      end
 
+    def edit
+        @article = Article.find(params[:id])
     end
-
+       
     def create
         @article = Article.new(article_params)
        
-        @article.save
-        redirect_to @article
+        if @article.save
+          redirect_to @article
+        else
+          render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
+    end
+
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+   
+        redirect_to articles_path
     end
        
     private def article_params
